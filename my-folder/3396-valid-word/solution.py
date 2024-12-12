2d14ae36e0bdd3a1283e072d56b52cd8b2bf4136
Colin Melville <94224928+ctmes@users.noreply.github.com>
2024-12-12 02:57:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.2 MB (29.72%)

--- my-folder/3396-valid-word/solution.py
+++ my-folder/3396-valid-word/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def isValid(self, word: str) -> bool:
         if not word:
-            print('1')
             return False
         
         if len(word) < 3:
-            print('2')
             return False
 
         for char in word:
             if char.isalnum() == False:
-                print('3')
                 return False
 
         vowels = "aeiou"
@@ -18,13 +15,10 @@ class Solution:
 
         # vowels > 0
         if len(set(word.lower()).intersection(vowels)) == 0:
-            print(len(set(word).intersection(vowels)))
-            print('4')
             return False
 
         # consonants > 0
         if len(set(word.lower()).difference(vowels2)) == 0:
-            print('5')
             return False
         
         return True
