07569ef1fa56695143d94ba434acb0f70d9c55fa
Colin Melville <94224928+ctmes@users.noreply.github.com>
2024-12-28 15:32:43 +0000

    [LeetCode Sync] Runtime - 27 ms (30.13%), Memory - 18.1 MB (5.78%)

--- my-folder/0383-ransom-note/solution.py
+++ my-folder/0383-ransom-note/solution.py
@@ -1,8 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for r in ransomNote:
-            if (r not in magazine or magazine.count(r) < ransomNote.count(r)):
-                return False
+        s = {}
+
+        for char in magazine:
+            if char in s:
+                s[char] += 1
+            else:
+                s[char] = 1
         
+        for char in ransomNote:
+            if char in s:
+                s[char] -= 1
+            else:
+                return False
+
+        for char in s:
+            if s[char] < 0:
+                return False
+
         return True
         
