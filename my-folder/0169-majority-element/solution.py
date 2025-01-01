f4a1c3bec3e801d8b36ee1bcef3cae95b0743820
Colin Melville <94224928+ctmes@users.noreply.github.com>
2025-01-01 03:02:37 +0000

    [LeetCode Sync] Runtime - 15 ms (18.14%), Memory - 19.4 MB (12.57%)

--- my-folder/0169-majority-element/solution.py
+++ my-folder/0169-majority-element/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        occurrences = {}
-        for i in nums:
-            if i in occurrences:
-                occurrences[i] += 1
+        S = {}
+        L = len(nums) // 2
+        
+        for num in nums:
+            if num in S:
+                if S[num] <= L:
+                    S[num] += 1
             else:
-                occurrences[i] = 1
-
-        majority = max(occurrences, key=occurrences.get)
-        return majority
+                S[num] = 1
+        
+        return max(S, key=S.get)
         
