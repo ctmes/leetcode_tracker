5696af4cf0771fdae6a1e0e0773810eda1c13f80
Colin Melville <94224928+ctmes@users.noreply.github.com>
2025-04-15 06:12:43 +0000

    [LeetCode Sync] Runtime - 71 ms (82.02%), Memory - 27.2 MB (95.72%)

--- my-folder/0643-maximum-average-subarray-i/solution.py
+++ my-folder/0643-maximum-average-subarray-i/solution.py
@@ -3,9 +3,8 @@ class Solution:
         curr_sum = sum(nums[:k])
         max_sum = curr_sum
         
-        # Slide the window
         for i in range(k, len(nums)):
-            curr_sum = curr_sum + nums[i] - nums[i - k]
+            curr_sum = curr_sum - nums[i - k] + nums[i]
             max_sum = max(max_sum, curr_sum)
         
         return max_sum / k
