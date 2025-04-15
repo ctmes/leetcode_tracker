e79cad60cb2ce77203e7080014e8bb525ecff1a1
Colin Melville <94224928+ctmes@users.noreply.github.com>
2025-04-15 16:34:56 +0000

    [LeetCode Sync] Runtime - 4 ms (7.50%), Memory - 17.7 MB (46.54%)

---
+++ my-folder/3379-score-of-a-string/solution.py
@@ -0,0 +1,10 @@
+import math
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        
+        for i in range(1, len(s)):
+            score += abs(ord(s[i]) - ord(s[i-1]))
+            
+        return score
