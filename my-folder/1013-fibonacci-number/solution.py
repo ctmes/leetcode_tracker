90b640b74e4ba9b588bce72c0d822a0be99b9888
Colin Melville <94224928+ctmes@users.noreply.github.com>
2025-04-23 12:21:12 +0000

    [LeetCode Sync] Runtime - 36 ms (78.36%), Memory - 17.8 MB (24.09%)

--- my-folder/1013-fibonacci-number/solution.py
+++ my-folder/1013-fibonacci-number/solution.py
@@ -1,15 +1,27 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
-            return n
+        # memo = {0:0, 1:1}
 
-        p, c = 0, 1
+        # def f(x):
+        #     if x in memo:
+        #         return memo[x]
 
-        for _ in range(2, n+1):
-            p, c = c, p + c
+        #     else:
+        #         memo[x] =  f(x-1) + f(x-2)
+        #         return memo[x]
 
-        return c
-        
+        # return f(n)
 
+        if n == 0:
+            return 0
 
-        
+        if n == 1:
+            return 1
+
+        prev = 0
+        cur = 1
+
+        for i in range(2, n+1):
+            prev, cur = cur, prev+cur
+
+        return cur
